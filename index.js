129c0e9f0b8d18bb267467063c5190fa3f2d08ac
Wyatt Pearsall <wyattttttt@gmail.com>
2015-02-07 15:23:05 -0800

    Better internals

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 function col (str){
   str
     ? col.obj[str] = this
-    : col.obj.anon.push(this)
+    : col.obj.anon
+      ? col.obj.anon.push(this)
+      : col.obj.anon = [this]
     ;
   return this;
 }
 
-col.obj = {anon:[]};
+col.obj = {};
 col.dump = function(){
   var ret;
-  if(Object.keys(col.obj).length===1){
+  if(col.obj.anon&&Object.keys(col.obj).length===1){
     if(col.obj.anon.length===1) ret = col.obj.anon[0];
     else ret = col.obj.anon;
   }else{
     ret = col.obj;
   }
-  col.obj = {anon:[]};
+  col.obj = {};
   return ret; 
 }
 
